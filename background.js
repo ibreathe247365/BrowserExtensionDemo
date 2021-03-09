1c9959d6e2fd535e098898c1ddfa8fdd2beb1098
Samir Sharma <samirsharma.iitd@gmail.com>
2021-03-10 00:40:12 +0530

    another commit

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
+window.bears={}
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-    bears[request.url]=response.count
+    window.bears[request.url]=response.count
 })
 
 chrome.browserAction.onClicked.addListener(function (tab){
